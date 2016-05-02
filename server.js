417579e5f0ba4a504e868d39a38a92156f3e6d1a
Anto <anto.aravinth.cse@gmail.com>
2016-05-02 14:27:40 +0530

    Completed complete REST API for folders and bookmarks

--- server.js
+++ server.js
@@ -7,11 +7,10 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 
 router.get('/', function(req, res) {
-    // res.json({ message: 'Hello world' });   
     res.sendfile('./public/index.html');
 });
 
-//REST API for bookmarks
+//REST API for folders
 router.route('/folders')
 	  .post(function(req,res){
 	  		var folder = new Folder();
@@ -32,8 +31,39 @@ router.route('/folders')
 		  			res.json(folders)
 		  	})
 	  })
+
+//REST for single documents
+router.route('/folders/:foldername')
+	.get(function(req,res){
+		  	Folder.find({name:req.params.foldername},function(err,folder){
+		  		if(err)
+		  			res.send(err);
+		  		else
+		  			res.json(folder)
+		  	})
+	  })
+	  .put(function(req,res){
+		  	Folder.find({name:req.params.foldername},function(err,folder){
+		  		if(err)
+		  			res.send(err);
+
+		  		if(folder.length != 0)
+		  		{
+			  		folder[0].name = req.body.foldername;
+
+			  		folder[0].save(function(err){
+			  			if(err)
+			  				res.send(err);
+			  			
+			  			res.json({ message: 'Folder updated successfully' });
+			  		})
+			  	} 
+			  	else
+			  		res.json({ message: 'Folder not found' });
+		  	})
+	  })
 	  .delete(function(req,res){
-	  		Folder.find({name:req.body.foldername})
+	  		Folder.find({name:req.params.foldername})
 	  			  .remove(function(err,success){
 	  			  		if(err)
 	  			  			res.send(err);
@@ -42,34 +72,101 @@ router.route('/folders')
 	  			  })
 	  })
 
+//REST API for bookmarks
 router.route('/bookmarks')
 	  .post(function(req,res){
 
-	  		var query = {name : req.body.foldername} , updatedBookmarks ,
+	  		var query = {name : req.body.foldername} , updatedBookmarksRef ,
 	  			bookmark = new Bookmark();
 
-			Folder.findOne(query,'bookmarks',function(err, oldBookmarks){
+			Folder.findOne(query,'bookmarks',function(err, folder){
 
-				if(err || oldBookmarks === null)
+				if(err || folder === null)
 					return res.send({error: "Folder not found."});
 
-				updatedBookmarks = oldBookmarks
+				updatedBookmarksRef = folder.bookmarks
 				bookmark.name = req.body.bookmarkname;
 	  			bookmark.url = req.body.url;
 
-	  			updatedBookmarks.bookmarks.push(bookmark)
-	  			console.log(updatedBookmarks)
+	  			updatedBookmarksRef.push(bookmark)
+	  			console.log(updatedBookmarksRef)
 
 				Folder.findOneAndUpdate(
 	  			query,
-	  			{ $set: { "bookmarks": updatedBookmarks.bookmarks } }, 
+	  			{ $set: { "bookmarks": updatedBookmarksRef } }, 
 	  			{upsert:true}, 
 	  			function(err, doc){
 				    if (err) return res.send(err);
-				    return res.send("succesfully saved");
+				    return res.json({message : "Bookmark saved successfully"});
 				});
 			})
 		})
+
+//REST API for single documents
+router.route('/folders/:foldername/bookmarks/:bookmarkid')
+	  .delete(function(req,res){
+	  		var query = {name : req.params.foldername} , bookmark;
+
+			Folder.findOne(query,'bookmarks',function(err, folder){
+
+				if(err || folder === null)
+					return res.send({error: "Folder not found."});
+
+				bookmark = folder.bookmarks.id(req.params.bookmarkid);
+				if(bookmark != undefined || bookmark != null)
+				{
+					bookmark.remove();
+					folder.save(function (err) {
+				  	if(err)
+				  		res.send(err);
+				  	  else
+	  			  		res.json({message : 'Bookmark deleted successfully'});
+					});
+				}
+				else
+					res.json({error: 'Bookmark not found'})
+			})
+	  })
+	  .get(function(req,res){
+	  		var query = {name : req.params.foldername} , bookmark;
+
+			Folder.findOne(query,'bookmarks',function(err, folder){
+
+				if(err || folder === null)
+					return res.send({error: "Folder not found."});
+
+				bookmark = folder.bookmarks.id(req.params.bookmarkid);
+				if(bookmark != undefined || bookmark != null) 
+					res.send(bookmark)
+				else
+					res.json({error: 'Bookmark not found'})
+			})
+	  })
+	  .put(function(req,res){
+	  		var query = {name : req.params.foldername} , updatedBookmarksRef ,
+	  			bookmark = new Bookmark();
+
+			Folder.findOne(query,'bookmarks',function(err, folder){
+
+				if(err || folder === null)
+					return res.send({error: "Folder not found."});
+
+				bookmark = folder.bookmarks.id(req.params.bookmarkid);
+				if(bookmark != undefined || bookmark != null)
+				{
+					bookmark.name = req.body.bookmarkname;
+					bookmark.url = req.body.url;
+					folder.save(function (err) {
+				  	if(err)
+				  		res.send(err);
+				  	  else
+	  			  		res.json({message : 'Bookmark updated successfully'});
+					});
+				}
+				else
+					res.json({error: 'Bookmark not found'})
+			})
+	  })
 
 //routes will be prefixed with apis
 app.use('/api', router);
